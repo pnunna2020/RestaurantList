bdecb89bac97494c2fa5bdef1af3c4bfee9d9546
Pattabhi Nunna <pnunna2016@gmail.com>
2020-05-12 16:45:39 -0400

    new test

---
+++ src/test/java/com/restaurants/web/RestaurantTest.java
@@ -0,0 +1,17 @@
+ 
+package com.restaurants.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//import com.restaurants;
+
+public class RestaurantTest {
+
+	@Test
+	public void testLengthOfTheUniqueKey() {
+
+		//Greeting msg = new Greeting();
+		Assert.assertEquals("GOOD", "BAD");
+	}
+}
